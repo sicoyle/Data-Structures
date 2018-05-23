68328203b7974db6b4b4d5cad3e6c8b61d875195
Sam <sic14@txstate.edu>
2018-05-23 10:53:11 -0500

    Fill in code assignment

---
+++ homework4_1/cmake-build-debug/CMakeFiles/homework4_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/homework4_1.dir/main.cpp.obj"
+  "homework4_1.pdb"
+  "homework4_1.exe"
+  "homework4_1.exe.manifest"
+  "libhomework4_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/homework4_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
