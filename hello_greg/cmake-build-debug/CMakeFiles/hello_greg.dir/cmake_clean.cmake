a6119b40f1d32eb25b9586fc02f37e80fa995326
Sam <sic14@txstate.edu>
2018-05-23 10:50:12 -0500

    Hello world program to make sure CLion works

---
+++ hello_greg/cmake-build-debug/CMakeFiles/hello_greg.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_greg.dir/main.cpp.obj"
+  "hello_greg.pdb"
+  "hello_greg.exe"
+  "hello_greg.exe.manifest"
+  "libhello_greg.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_greg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ hello_greg/main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main() {
+  std::cout << "Hello, Greg!" << std::endl;
+  return 0;
+}
