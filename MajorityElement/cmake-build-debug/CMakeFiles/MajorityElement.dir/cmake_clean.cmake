d4c0943095c34dcfe76f4bdae1b883e32935ab6a
Sam <sic14@txstate.edu>
2018-05-23 11:05:37 -0500

    Find majority element

---
+++ MajorityElement/cmake-build-debug/CMakeFiles/MajorityElement.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MajorityElement.dir/main.cpp.obj"
+  "MajorityElement.pdb"
+  "MajorityElement.exe"
+  "MajorityElement.exe.manifest"
+  "libMajorityElement.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MajorityElement.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
